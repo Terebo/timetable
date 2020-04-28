784af2ddc102ddf050d2b017d0c8899e84f3c86d
Terebo <63092966+Terebo@users.noreply.github.com>
2020-04-28 09:42:59 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+function getTimes() {
+	var alltimes = document.getElementsByName("time");
+	var timezoneselect = document.getElementById("timezoneselect");
+	var i = -1;
+	alltimes.forEach(function () {
+		i = i + 1;
+		var time = parseInt(alltimes[i]["className"], 10);
+		console.log(typeof(time0));
+		time = time + parseInt(timezoneselect.value, 10);
+		console.log(time);
+		temp = time + 2;
+		var tempadditive = " AM";
+		if (temp > 12) {
+			temp = temp - 12;
+			tempadditive = " PM";
+		}
+		if (temp >= 24) {
+			temp = temp - 24;
+			tempadditive = " AM";
+		}
+		temp = temp + tempadditive;
+		alltimes[i]["innerText"] = time + " - " + temp;
+	});
+
+}
