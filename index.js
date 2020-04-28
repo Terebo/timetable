b394340ae3847a22e81d094d114e3e52348e3e78
Terebo <63092966+Terebo@users.noreply.github.com>
2020-04-28 09:57:33 +0200

    fixed another overflow bug

--- index.js
+++ index.js
@@ -5,18 +5,21 @@ function getTimes() {
 	alltimes.forEach(function () {
 		i = i + 1;
 		var time = parseInt(alltimes[i]["className"], 10);
+		if (time == 23) {
+		alltimes[i]["innerText"] = 11 + " - " + 1 + "AM";}
+		else {
 		console.log(typeof(time0));
 		time = time + parseInt(timezoneselect.value, 10);
 		console.log(time);
 		temp = time + 2;
 		var tempadditive = " AM";
-		if (time > 12) {
+		if (time > 12 && time < 24) {
 			time = time - 12;
 		}
 		if (time >= 24) {
 			time = time - 24;
 		}
-		if (temp > 12) {
+		if (temp > 12 && temp < 24) {
 			temp = temp - 12;
 			tempadditive = " PM";
 		}
@@ -26,6 +29,6 @@ function getTimes() {
 		}
 		temp = temp + tempadditive;
 		alltimes[i]["innerText"] = time + " - " + temp;
-	});
+		}});
 
 }
